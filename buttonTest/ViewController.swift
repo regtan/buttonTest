48fa7804da3be68ed7d41c6c98850ada67c419cb
regtan <regtan1210@gmail.com>
2016-10-10 23:50:28 +0900

    add button

--- buttonTest/ViewController.swift
+++ buttonTest/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myLabel: UILabel!
 
+    @IBAction func tapButton() {
+        myLabel.text = "こんばんは"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
